7f7fae9c49fd5c183bdf2ddc88851bccd4959f9b
Bufovsky <39122181+Bufovsky@users.noreply.github.com>
2020-08-13 23:02:45 +0200

    V 0.1
    Full aplication

---
+++ index.php
@@ -0,0 +1,19 @@
+<?PHP
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once( __DIR__ . '/system/config.php' );
+
+include_once( __DIR__ . '/system/database.php' );
+
+include_once( __DIR__ . '/system/session.php' );
+
+include_once( __DIR__ . '/system/functions.php' );
+
+include_once( __DIR__ . '/system/pages.php' );
+
+include_once( __DIR__ . '/theme/design.php' );
+
+?>
